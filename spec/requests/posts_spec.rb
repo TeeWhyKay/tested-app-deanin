4291927f7b082c827628a35fa93ac6818b9a14ab
YongKeeTan <yongkee.t@gmail.com>
2021-11-09 17:21:56 +0800

    update valid and invalid attributes

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,16 +13,25 @@
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/posts", type: :request do
-  
+  current_user = User.first_or_create(email: 'test@test.com', password: '123123', password_confirmation: '123123')
   # Post. As you add validations to Post, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    {
+      'id': '1',
+      'title': 'Test',
+      'body': '12345',
+      'user': current_user
+    }
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    {
+      'id': 'a',
+      'title': '1',
+      'body': '1234'
+    }
+  end
 
   describe "GET /index" do
     it "renders a successful response" do
